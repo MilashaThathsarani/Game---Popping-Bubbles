23dc7b7bb6fcb4e45f6ba93ee016f901441f30db
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-04-10 19:58:55 +0530

    Content Updated

---
+++ js/style.js
@@ -0,0 +1,12 @@
+let bubbles = ['one', 'two', 'three','four' ,'five'];
+let windowWidth = window.innerWidth;
+let body = document.body;
+let windowHeight = window.innerHeight;
+let scores = document.querySelectorAll('.score');
+let noPop = 0;
+let total = 30;
+let currentBubble = 0;
+let gameOver = false;
+let shadow = document.querySelector('.shadow');
+let startBtn = document.querySelector('.start-btn');
+
